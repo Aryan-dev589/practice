d824f5a5fb8ff5832c76848936fcc806ff406db2
Aryan-dev589 <mishraaryan0004@gmail.com>
2024-11-04 14:23:26 +0530

    Enter armstrong

--- Placement/armstrong.java
+++ Placement/armstrong.java
@@ -7,33 +7,24 @@ public class armstrong {
         int num=sc.nextInt();
 
         int rem,count=0,sum=0;
-    int temp=num;
-                       
-
-        
+        int temp=num;
 
         while (temp!=0) { 
             temp=temp/10;
             count=count+1;
-}
- temp=num;
-          while (temp!= 0) {
+        }
+         temp=num;
+        while (temp!= 0) {
             rem=temp%10;
             sum=sum+(int)Math.pow(rem,count);
             temp=temp/10;
-}
+        }
 
-
-            
-        
         if (sum==num) {
             System.out.println("armstrong number");
         }
-        
-        
          else{
             System.out.println("not armstrong");
-        
         }
         sc.close();
 
